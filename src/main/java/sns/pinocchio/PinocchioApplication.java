8e8c6b75c8d0c5e4e4bd26179f7bb816b3526874
Hoon <xianse10@gmail.net>
2025-03-25 16:10:49 +0900

    Initial commit

---
+++ src/main/java/sns/pinocchio/PinocchioApplication.java
@@ -0,0 +1,13 @@
+package sns.pinocchio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinocchioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinocchioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sns/pinocchio/PinocchioApplicationTests.java
@@ -0,0 +1,13 @@
+package sns.pinocchio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinocchioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

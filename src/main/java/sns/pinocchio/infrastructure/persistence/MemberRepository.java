39634b965091dc30a6c91a243c010f0aaa64ecae
gorhkd <gorhkd223@gmail.com>
2025-03-26 10:30:47 +0900

    feat : Member 컨트롤러 API명세서 반영

--- src/main/java/sns/pinocchio/infrastructure/persistence/MemberRepository.java
+++ src/main/java/sns/pinocchio/infrastructure/persistence/MemberRepository.java
@@ -1,7 +1,9 @@
 package sns.pinocchio.infrastructure.persistence;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import sns.pinocchio.domain.member.Member;
 
 @Repository
-public interface MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
--- src/main/java/sns/pinocchio/presentation/member/MemberController.java
+++ src/main/java/sns/pinocchio/presentation/member/MemberController.java
@@ -1,7 +1,68 @@
 package sns.pinocchio.presentation.member;
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sns.pinocchio.application.member.MemberService;
 
-@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
+@RestController
 public class MemberController {
+
+    private final MemberService memberService;
+
+    // 회원 정보 조회
+    @GetMapping("/{memberId}")
+    public ResponseEntity<String> getMemberInfo(@PathVariable Long memberId) {
+        return ResponseEntity.ok("회원 정보 조회 성공");
+    }
+
+    // 회원 정보 수정
+    @PutMapping("/{memberId}")
+    public ResponseEntity<String> updateMemberInfo(@PathVariable Long memberId) {
+        return ResponseEntity.ok("회원 정보 수정 성공");
+    }
+
+    // 임시 비밀번호 발송
+    @PostMapping("/{memberId}/password/reset")
+    public ResponseEntity<String> sendTemporaryPassword(@PathVariable Long memberId) {
+        return ResponseEntity.ok("임시 비밀번호 발송 성공");
+    }
+
+    // 비밀번호 변경
+    @PutMapping("/{memberId}/password")
+    public ResponseEntity<String> changePassword(@PathVariable Long memberId) {
+        return ResponseEntity.ok("비밀번호 변경 성공");
+    }
+
+    // 회원 탈퇴
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity<String> deleteMember(@PathVariable Long memberId) {
+        return ResponseEntity.ok("회원 탈퇴 성공");
+    }
+
+    // 계정 신고
+    @PostMapping("/report")
+    public ResponseEntity<String> reportMember(@RequestParam Long targetMemberId) {
+        return ResponseEntity.ok("계정 신고 성공");
+    }
+
+    // 계정 차단
+    @PostMapping("/block")
+    public ResponseEntity<String> blockMember(@RequestParam Long targetMemberId) {
+        return ResponseEntity.ok("계정 차단 성공");
+    }
+
+    // 계정 차단 해제
+    @PostMapping("/unblock")
+    public ResponseEntity<String> unblockMember(@RequestParam Long targetMemberId) {
+        return ResponseEntity.ok("계정 차단 해제 성공");
+    }
+
+    // 차단한 유저 조회
+    @GetMapping("/block")
+    public ResponseEntity<String> getBlockedMembers(@RequestParam Long memberId) {
+        return ResponseEntity.ok("차단한 유저 조회 성공");
+    }
 }

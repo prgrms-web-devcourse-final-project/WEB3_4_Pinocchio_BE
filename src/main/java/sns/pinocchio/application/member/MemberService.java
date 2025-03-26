5b926215bb9b52adf14467ef182f6fac3b6f24f5
gorhkd <gorhkd223@gmail.com>
2025-03-27 00:22:26 +0900

    feat : 로그인, 로그아웃 Redis 적용

--- src/main/java/sns/pinocchio/application/member/MemberService.java
+++ src/main/java/sns/pinocchio/application/member/MemberService.java
@@ -1,15 +1,19 @@
 package sns.pinocchio.application.member;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import sns.pinocchio.application.member.memberDto.MemberInfoDto;
 import sns.pinocchio.application.member.memberDto.MemberRequestDto;
 import sns.pinocchio.config.global.auth.service.CookieService;
+import sns.pinocchio.config.global.auth.util.JwtUtil;
 import sns.pinocchio.config.global.auth.util.TokenProvider;
 import sns.pinocchio.domain.member.Member;
 import sns.pinocchio.infrastructure.member.MemberRepository;
+import sns.pinocchio.infrastructure.persistence.redis.redisService.RedisService;
 import sns.pinocchio.presentation.member.exception.MemberErrorCode;
 import sns.pinocchio.presentation.member.exception.MemberException;
 
@@ -20,7 +24,9 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final CookieService cookieService;
+    private final RedisService redisService;
     private final TokenProvider tokenProvider;
+    private final JwtUtil jwtUtil;
 
     // 계정 생성
     @Transactional
@@ -43,7 +49,7 @@ public class MemberService {
 
     // 이메일 검증
     public void validateEmail(String email) {
-        if(this.memberRepository.existsByEmail(email)) {
+        if(memberRepository.existsByEmail(email)) {
             return;
         }
         throw new MemberException(MemberErrorCode.EMAIL_NOT_FOUND);
@@ -51,7 +57,7 @@ public class MemberService {
 
     // 패스워드 검증
     public void validatePassword(String password, String email) {
-        Member member = this.memberRepository.findByEmail(email);
+        Member member = memberRepository.findByEmail(email);
 
         if(passwordEncoder.matches(password, member.getPassword())) {
             return;
@@ -61,20 +67,27 @@ public class MemberService {
 
     // 엑세스토큰 생성 및 저장
     public void generateToken(String email, HttpServletResponse response) {
-        Member member = this.memberRepository.findByEmail(email);
-        String accessToken = this.tokenProvider.generateAccessToken(member);
+        Member member = memberRepository.findByEmail(email);
+        String accessToken = tokenProvider.generateAccessToken(member);
         this.cookieService.addAccessTokenToCookie(accessToken, response);
     }
 
     // 리프래시토큰 생성 및 저장
-    public void generateAndSaveRefreshToken(HttpServletResponse response) {
-        String refreshToken = this.tokenProvider.generateRefreshToken();
+    public void generateAndSaveRefreshToken(String email, HttpServletResponse response) {
+        Member member = memberRepository.findByEmail(email);
+        String refreshToken = tokenProvider.generateRefreshToken();
+
+        this.redisService.save(refreshToken, String.valueOf(member.getId()), jwtUtil.getRefreshTokenExpirationTime());
         this.cookieService.addRefreshTokenToCookie(refreshToken, response);
 
     }
 
     // 토큰 제거
-    public void logout(HttpServletResponse response) {
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        String accessToken = this.cookieService.getAccessTokenFromCookie(request);
+        MemberInfoDto member = this.jwtUtil.getMemberInfoDto(accessToken);
+
+        this.redisService.addBlackList(String.valueOf(member.id()), this.jwtUtil.getRefreshTokenExpirationTime());
         this.cookieService.clearTokenFromCookie(response);
     }
 

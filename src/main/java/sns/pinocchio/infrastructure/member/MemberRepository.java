bdfb4d4f457b1abbc887c600049bc7354c3cbca8
gorhkd <gorhkd223@gmail.com>
2025-03-26 10:43:07 +0900

    chore : 패키지명 수정

--- src/main/java/sns/pinocchio/infrastructure/member/MemberRepository.java
+++ src/main/java/sns/pinocchio/infrastructure/member/MemberRepository.java
@@ -1,4 +1,4 @@
-package sns.pinocchio.infrastructure.persistence;
+package sns.pinocchio.infrastructure.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;

5f0209f1713f04e15638e1ab169ce3330348d8a7
UCLABgown <lovecein4858@gmail.com>
2025-03-26 16:43:15 +0900

    feat - commentLikeService에 setCommentLike 추가

---
+++ src/main/java/sns/pinocchio/application/comment/CommentLikeService.java
@@ -0,0 +1,33 @@
+package sns.pinocchio.application.comment;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import sns.pinocchio.domain.comment.CommentLike;
+import sns.pinocchio.infrastructure.persistence.mongodb.CommentLikeRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CommentLikeService {
+	private final CommentLikeRepository commentLikeRepository;
+
+	public Optional<String> setCommentLike(String commentId,String loginUserId){
+		Optional<CommentLike> optCommentLike = commentLikeRepository.findByUserIdAndCommentId(loginUserId, commentId);
+		if(optCommentLike.isEmpty()){
+			CommentLike commentLike = CommentLike.builder()
+				.commentId(commentId)
+				.userId(loginUserId)
+				.createdAt(LocalDateTime.now())
+				.build();
+			String commentLikeId = commentLikeRepository.save(commentLike).getId();
+			return Optional.ofNullable(commentLikeId);
+		} else{
+			CommentLike commentLike = optCommentLike.get();
+			commentLikeRepository.delete(commentLike);
+			return Optional.empty();
+		}
+	}
+}

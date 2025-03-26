4a83751d9a458be92d1babcfe00c8d78bcc21d8b
gorhkd <gorhkd223@gmail.com>
2025-03-26 10:31:39 +0900

    feat : member 회원가입 DTO 생성

---
+++ src/main/java/sns/pinocchio/presentation/member/memberDto/MemberRequestDto.java
@@ -0,0 +1,31 @@
+package sns.pinocchio.presentation.member.memberDto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberRequestDto {
+
+    @NotBlank(message = "이름은 필수 항목입니다.")
+    private String name;
+
+    @NotBlank(message = "이메일은 필수 항목입니다.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    private String email;
+
+    @NotBlank(message = "닉네임은 필수 항목입니다.")
+    @Size(min = 3, max = 20, message = "닉네임은 3자 이상 20자 이하로 입력해주세요.")
+    private String nickname;
+
+    @NotBlank(message = "비밀번호는 필수 항목입니다.")
+    @Size(min = 8, message = "비밀번호는 8자 이상이어야 합니다.")
+    private String password;
+}

1df2db664af9e8d1e68a5100f8a24f5c858f9900
UCLABgown <lovecein4858@gmail.com>
2025-03-26 15:24:11 +0900

    feat - comment service에 댓글 수정기능 추가

--- src/main/java/sns/pinocchio/application/comment/CommentService.java
+++ src/main/java/sns/pinocchio/application/comment/CommentService.java
@@ -45,4 +45,11 @@ public class CommentService {
 			}
 		}
 	}
+
+	public void modifyComment(CommentModifyRequest request,String loginUserId){
+		Comment comment = commentRepository.findByIdAndUserIdAndPostId(request.commentId, loginUserId, request.postId)
+			.orElseThrow(() -> new NoSuchElementException("등록된 댓글을 찾을 수 없습니다."));
+		comment.setContent(request.content);
+		commentRepository.save(comment);
+	}
 }

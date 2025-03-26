b4f3754393c8322f21438a18c23bf9df3e9531a0
UCLABgown <lovecein4858@gmail.com>
2025-03-26 14:33:42 +0900

    feat - comment service 댓글 생성 기능 추가

--- src/main/java/sns/pinocchio/application/comment/CommentService.java
+++ src/main/java/sns/pinocchio/application/comment/CommentService.java
@@ -1,7 +1,33 @@
 package sns.pinocchio.application.comment;
 
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
+
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+import sns.pinocchio.domain.comment.Comment;
+import sns.pinocchio.domain.comment.CommentStatus;
+import sns.pinocchio.infrastructure.persistence.mongodb.CommentRepository;
+
 @Service
+@RequiredArgsConstructor
 public class CommentService {
+	private final CommentRepository commentRepository;
+
+	public String createComment(CommentCreateRequest request, String userId, String postId) {
+		Comment comment = Comment.builder()
+			.userId(userId)
+			.postId(postId)
+			.content(request.getContent())
+			.parentCommentId(request.getParentCommentId())
+			.likes(0)
+			.createdAt(LocalDateTime.now())
+			.updatedAt(LocalDateTime.now())
+			.status(CommentStatus.ACTIVE)
+			.build();
+
+		Comment savedComment = commentRepository.save(comment);
+		return savedComment.getId();
+	}
 }

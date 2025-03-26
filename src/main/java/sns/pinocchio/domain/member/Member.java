6572e2f7f3feb71aca0e277bebe7cac92aedd4a3
gorhkd <gorhkd223@gmail.com>
2025-03-26 10:30:28 +0900

    feat : Member 엔티티 생성

--- src/main/java/sns/pinocchio/domain/member/Member.java
+++ src/main/java/sns/pinocchio/domain/member/Member.java
@@ -1,9 +1,45 @@
 package sns.pinocchio.domain.member;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    private String bio;
+
+    private String website;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String profileImageUrl;
+
+    private Boolean isActive;
+
+    @Builder
+    public Member(String password, String email, String name, String nickname) {
+        this.password = password;
+        this.email = email;
+        this.name = name;
+        this.nickname = nickname;
+    }
 }

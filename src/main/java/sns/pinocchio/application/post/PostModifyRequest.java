cb957246b5798171f0ff1df48885281d9a428011
Hoon <xianse10@gmail.net>
2025-03-26 17:41:13 +0900

    post 수정 컨트롤러 및 스웨거 등록 api 테스트 시 PostModifyRequest 참조

--- src/main/java/sns/pinocchio/application/post/PostModifyRequest.java
+++ src/main/java/sns/pinocchio/application/post/PostModifyRequest.java
@@ -1,5 +1,6 @@
 package sns.pinocchio.application.post;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.util.List;
@@ -8,11 +9,21 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Builder// 테스트를 위한 빌더
+//(api 테스트시 postId를 따로 넣어야 할것)
 public class PostModifyRequest {
+    @Schema(hidden = true)
     private String postId;
-    private String userId; // 작성자
+
+    @Schema(hidden = true)
+    private String userId;
+
+    @Schema(description = "수정할 본문 내용", example = "수정된 게시글입니다.")
     private String content;
+
+    @Schema(description = "수정할 이미지 URL 리스트", example = "[\"https://img.com/updated.jpg\"]")
     private List<String> imageUrls;
-    private String visibility; // "public" or "private"
+
+    @Schema(description = "공개 범위 (public/private)", example = "private")
+    private String visibility;
 }

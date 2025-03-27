6893814c7db848c44595836aae51172503d30cca
sungjae <dtc13579@gmail.com>
2025-03-27 15:16:00 +0900

    test: 알림 조회 API 단위 테스트 추가

--- src/test/java/sns/pinocchio/application/notification/service/NotificationServiceTest.java
+++ src/test/java/sns/pinocchio/application/notification/service/NotificationServiceTest.java
@@ -1,6 +1,7 @@
 package sns.pinocchio.application.notification.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -14,7 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import sns.pinocchio.application.notification.dto.NotificationRequestDto.UpdateNotifications;
 import sns.pinocchio.application.notification.dto.NotificationResponseDto.NotificationInfo;
 import sns.pinocchio.domain.notification.Notification;
-import sns.pinocchio.infrastructure.persistence.mongodb.NotificationRepository;
+import sns.pinocchio.infrastructure.persistence.mysql.NotificationRepository;
 
 @ExtendWith(MockitoExtension.class)
 class NotificationServiceTest {
@@ -26,7 +27,7 @@ class NotificationServiceTest {
   private final String userId = "test_user_123";
 
   @Test
-  @DisplayName("알림 설정: 기존 설정이 없는 경우")
+  @DisplayName("알림 설정 Success: 기존 설정이 없는 경우")
   void updateNotificationsNotExistTest() {
 
     // given
@@ -56,13 +57,13 @@ class NotificationServiceTest {
   }
 
   @Test
-  @DisplayName("알림 설정: 기존 설정이 있는 경우")
+  @DisplayName("알림 설정 Success: 기존 설정이 있는 경우")
   void updateNotificationsExistTest() {
 
     // given
     Notification existed =
         Notification.builder()
-            .id("notification_123")
+            .id(1L)
             .userId(userId)
             .message(false)
             .like(false)
@@ -94,5 +95,72 @@ class NotificationServiceTest {
     assertThat(updated.comment()).isTrue();
     assertThat(updated.follow()).isTrue();
     assertThat(updated.mention()).isTrue();
+  }
+
+  @Test
+  @DisplayName("알림 설정 조회 Success: 기존 알람 설정이 존재하지 않을 경우")
+  void getNotificationsNotExistTest() {
+
+    // given
+    when(notificationRepository.findByUserId(userId)).thenReturn(Optional.empty());
+
+    // when
+    NotificationInfo info = notificationService.getNotifications(userId);
+
+    // then
+    assertThat(info).isNotNull();
+    assertThat(info.userId()).isEqualTo(userId);
+    assertThat(info.message()).isFalse();
+    assertThat(info.like()).isFalse();
+    assertThat(info.comment()).isFalse();
+    assertThat(info.follow()).isFalse();
+    assertThat(info.mention()).isFalse();
+  }
+
+  @Test
+  @DisplayName("알림 설정 조회 Success: 기존 알람 설정이 존재할 경우")
+  void getNotificationsExistTest() {
+
+    // given
+    Notification existed =
+        Notification.builder()
+            .id(1L)
+            .userId(userId)
+            .message(false)
+            .like(true)
+            .comment(false)
+            .follow(true)
+            .mention(false)
+            .build();
+
+    when(notificationRepository.findByUserId(userId)).thenReturn(Optional.of(existed));
+
+    // when
+    NotificationInfo info = notificationService.getNotifications(userId);
+
+    // then
+    assertThat(info).isNotNull();
+    assertThat(info.userId()).isEqualTo(userId);
+    assertThat(info.message()).isFalse();
+    assertThat(info.like()).isTrue();
+    assertThat(info.comment()).isFalse();
+    assertThat(info.follow()).isTrue();
+    assertThat(info.mention()).isFalse();
+  }
+
+  @Test
+  @DisplayName("알림 설정 조회 Fail: 유저 ID가 존재하지 않을 경우")
+  void getNotificationsNoUserIdTest() {
+
+    // given
+    String errorMsg = "[userId] 정보가 존재하지 않습니다.";
+
+    // when
+    IllegalArgumentException exception =
+        assertThrows(
+            IllegalArgumentException.class, () -> notificationService.getNotifications(null));
+
+    // then
+    assertThat(exception.getMessage()).isEqualTo(errorMsg);
   }
 }

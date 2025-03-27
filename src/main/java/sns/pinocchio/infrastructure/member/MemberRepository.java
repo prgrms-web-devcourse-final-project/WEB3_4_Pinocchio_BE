99c5ea546012816666379a459b76d9be839811a5
gorhkd <gorhkd223@gmail.com>
2025-03-27 15:19:04 +0900

    refactor : 서비스 로직 예외 처리 추가

--- src/main/java/sns/pinocchio/infrastructure/member/MemberRepository.java
+++ src/main/java/sns/pinocchio/infrastructure/member/MemberRepository.java
@@ -4,11 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sns.pinocchio.domain.member.Member;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
 
-    Member findByEmail(String email);
+    Optional<Member> findByEmail(String email);
 
-    Member findByName(String username);
+    Optional<Member> findByName(String username);
 }

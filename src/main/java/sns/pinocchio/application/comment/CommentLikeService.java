ec7c20d7b09d5661cf9f3e4ed4b823e00f6b0038
UCLABgown <lovecein4858@gmail.com>
2025-03-28 16:06:39 +0900

    feat - 댓글 삭제시 좋아요 모두삭제 추가

--- src/main/java/sns/pinocchio/application/comment/CommentLikeService.java
+++ src/main/java/sns/pinocchio/application/comment/CommentLikeService.java
@@ -1,12 +1,14 @@
 package sns.pinocchio.application.comment;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import sns.pinocchio.domain.comment.CommentLike;
+import sns.pinocchio.domain.comment.CommentLikeStatus;
 import sns.pinocchio.infrastructure.persistence.mongodb.CommentLikeRepository;
 
 @Service
@@ -15,20 +17,36 @@ public class CommentLikeService {
 	private final CommentLikeRepository commentLikeRepository;
 
 	//댓글 좋아요 업데이트 함수 좋아요가이미 있을시 삭제 없을시 추가
-	public Optional<String> toggleCommentLike(String commentId,String loginUserId){
-		Optional<CommentLike> optCommentLike = commentLikeRepository.findByUserIdAndCommentId(loginUserId, commentId);
-		if(optCommentLike.isEmpty()){
+	public Optional<String> toggleCommentLike(String commentId, String loginUserTsid) {
+		Optional<CommentLike> optCommentLike = commentLikeRepository.findByUserTsidAndCommentId(loginUserTsid,
+			commentId);
+		if (optCommentLike.isEmpty()) {
 			CommentLike commentLike = CommentLike.builder()
 				.commentId(commentId)
-				.userId(loginUserId)
+				.userTsid(loginUserTsid)
 				.createdAt(LocalDateTime.now())
+				.status(CommentLikeStatus.ACTIVE)
 				.build();
 			String commentLikeId = commentLikeRepository.save(commentLike).getId();
 			return Optional.ofNullable(commentLikeId);
-		} else{
+		} else {
 			CommentLike commentLike = optCommentLike.get();
-			commentLikeRepository.delete(commentLike);
-			return Optional.empty();
+			CommentLikeStatus status = commentLike.getStatus();
+			boolean isActive = status == CommentLikeStatus.ACTIVE;
+			commentLike.setStatus(isActive ? CommentLikeStatus.DELETE : CommentLikeStatus.ACTIVE);
+			String commentLikeId = commentLikeRepository.save(commentLike).getId();
+			return isActive ?  Optional.empty():Optional.ofNullable(commentLikeId);
 		}
+	}
+
+	//댓글에 달린 모든 좋아요 삭제
+	public void deleteAllCommentlikes(String commentId){
+		commentLikeRepository.deleteByCommentId(commentId);
+	}
+
+	//유저의 좋아요 리스트 가져오기
+	public Map<String, Object> findLikesByUsers(String likeTsid) {
+		return Map.of("CommentLikeList",
+			commentLikeRepository.findAllByUserTsidAndStatus(likeTsid, CommentLikeStatus.ACTIVE));
 	}
 }

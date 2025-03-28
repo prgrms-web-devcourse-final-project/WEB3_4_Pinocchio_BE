050f79004b214cb0eb890f643643fda85a766e4a
UCLABgown <lovecein4858@gmail.com>
2025-03-28 16:09:00 +0900

    feat - 댓글 좋아요 레포 댓글로 삭제기능 및 누락된 조건 추가

--- src/main/java/sns/pinocchio/infrastructure/persistence/mongodb/CommentLikeRepository.java
+++ src/main/java/sns/pinocchio/infrastructure/persistence/mongodb/CommentLikeRepository.java
@@ -1,13 +1,18 @@
 package sns.pinocchio.infrastructure.persistence.mongodb;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import sns.pinocchio.domain.comment.Comment;
 import sns.pinocchio.domain.comment.CommentLike;
+import sns.pinocchio.domain.comment.CommentLikeStatus;
 
 public interface CommentLikeRepository extends MongoRepository<CommentLike, String> {
-	Optional<CommentLike> findByUserIdAndCommentId(String userId, String postId);
+	Optional<CommentLike> findByUserTsidAndCommentId(String userTsid, String postId);
+	long countByCommentIdAndStatus(String postId,CommentLikeStatus status);
+	List<CommentLike> findAllByUserTsidAndStatus(String userTsid, CommentLikeStatus status);
+	void deleteByCommentId(String commentId);
 
 }

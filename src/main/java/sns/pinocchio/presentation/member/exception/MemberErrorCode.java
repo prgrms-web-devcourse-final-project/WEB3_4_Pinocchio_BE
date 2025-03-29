4a90ba09b75265b9fa8d24e6f4627c5fe82a13da
gorhkd <gorhkd223@gmail.com>
2025-03-29 23:09:04 +0900

    feat : 중복 닉네임 에러 코드

--- src/main/java/sns/pinocchio/presentation/member/exception/MemberErrorCode.java
+++ src/main/java/sns/pinocchio/presentation/member/exception/MemberErrorCode.java
@@ -12,7 +12,8 @@ public enum MemberErrorCode {
     ACCOUNT_SUSPENDED(HttpStatus.FORBIDDEN, "USER_403", "정지된 계정입니다."),
     UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "USER_401", "로그인이 필요합니다."),
     EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_404", "이메일이 존재하지 않습니다."),
-    EMAIL_DUPLICATED(HttpStatus.BAD_REQUEST, "USER_409", "이미 사용 중인 이메일입니다.");
+    EMAIL_DUPLICATED(HttpStatus.BAD_REQUEST, "USER_409", "이미 사용 중인 이메일입니다."),
+    NICKNAME_DUPLICATED(HttpStatus.BAD_REQUEST, "USER_409", "이미 사용 중인 닉네임입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;

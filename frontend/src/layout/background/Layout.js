ff115c2d7e600154654b969765f26c11e1a6adbb
shson <sonsoohoon>
2025-03-31 18:10:00 +0900

    feat: FE 개발 커밋

--- frontend/src/layout/background/Layout.js
+++ frontend/src/layout/background/Layout.js
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import {Outlet} from "react-router-dom";
+import Footer from "./Footer";
 
 const Layout = () => {
     return (
         <div className={"kw"}>
             <Header />
             <Outlet />
+            <Footer />
         </div>
     );
 }

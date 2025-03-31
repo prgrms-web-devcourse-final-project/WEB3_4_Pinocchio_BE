91dd4c2422eb2e8b9c4fd10646e201a11238dbf9
gorhkd <gorhkd223@gmail.com>
2025-03-31 20:34:20 +0900

    feat : 리프레시토큰 쿠키 저장

---
+++ src/main/java/sns/pinocchio/config/global/auth/util/CookieUtil.java
@@ -0,0 +1,37 @@
+package sns.pinocchio.config.global.auth.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class CookieUtil {
+
+  public void addTokenToCookie(
+      String name, String value, long expiration, HttpServletResponse response) {
+    ResponseCookie cookie =
+        ResponseCookie.from(name, value)
+            .httpOnly(true)
+            .secure(true)
+            .path("/")
+            .sameSite("Strict")
+            .maxAge(expiration)
+            .build();
+
+    response.addHeader("Set-Cookie", cookie.toString());
+  }
+
+  public String getTokenFromCookie(String name, HttpServletRequest request) {
+    if (request.getCookies() == null) return null;
+
+    return Arrays.stream(request.getCookies())
+        .filter(cookie -> name.equals(cookie.getName()))
+        .map(Cookie::getValue)
+        .findFirst()
+        .orElse(null);
+  }
+}

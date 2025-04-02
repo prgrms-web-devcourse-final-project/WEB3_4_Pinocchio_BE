c62b3c74b4021d2685d0af33a3e55b0d3df6b2a8
gorhkd <gorhkd223@gmail.com>
2025-04-02 11:32:44 +0900

    feat : BlockController
    유저 차단, 차단 해제, 차단한 유저 조회

--- src/main/java/sns/pinocchio/presentation/block/BlockController.java
+++ src/main/java/sns/pinocchio/presentation/block/BlockController.java
@@ -1,11 +1,14 @@
 package sns.pinocchio.presentation.block;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import sns.pinocchio.application.blockedUser.BlockedUserService;
-import sns.pinocchio.application.blockedUser.request.BlockRequestDto;
+import sns.pinocchio.application.blockedUser.response.BlockedUserResponse;
+import sns.pinocchio.config.global.auth.model.CustomUserDetails;
 import sns.pinocchio.domain.member.Member;
 
 @RequestMapping("/block")
@@ -16,24 +19,43 @@ public class BlockController {
   private final BlockedUserService blockedUserService;
 
   // 유저 차단
-  @PostMapping("/user")
+  @PostMapping("/{userId}")
   public ResponseEntity<String> blockMember(
-      @RequestBody BlockRequestDto blockRequestDto, @AuthenticationPrincipal Member member) {
+      @PathVariable Long userId, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    Member member = customUserDetails.getMember();
+
+    blockedUserService.saveBlock(member.getId(), userId);
+
     return ResponseEntity.ok("해당 회원의 계정이 차단되었습니다.");
   }
 
   // 유저 차단 해제
-  @DeleteMapping("{userId}")
+  @DeleteMapping("/{userId}")
   public ResponseEntity<String> unblockMember(
-      @PathVariable Long memberId, @AuthenticationPrincipal Member member) {
+      @PathVariable Long userId, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    Member member = customUserDetails.getMember();
 
+    blockedUserService.deleteBlock(member.getId(), userId);
     return ResponseEntity.ok("해당 회원의 계정 차단이 해제되었습니다.");
   }
 
   // 차단한 유저 조회
-  @GetMapping("/users")
-  public ResponseEntity<String> getBlockedMembers(@AuthenticationPrincipal Member member) {
+  @GetMapping
+  public ResponseEntity<BlockedUserResponse> getBlockedMembers(
+      @AuthenticationPrincipal CustomUserDetails customUserDetails) {
 
-    return ResponseEntity.ok("차단한 회원 목록을 성공적으로 조회했습니다.");
+    Member member = customUserDetails.getMember();
+    List<BlockedUserResponse.UserData> blockedUsers =
+        blockedUserService.getBlockedUsers(member.getId());
+
+    BlockedUserResponse response =
+        BlockedUserResponse.builder()
+            .status("success")
+            .statusCode(HttpStatus.OK.value())
+            .message("차단한 회원 목록을 성공적으로 조회했습니다.")
+            .data(blockedUsers)
+            .build();
+
+    return ResponseEntity.ok(response);
   }
 }

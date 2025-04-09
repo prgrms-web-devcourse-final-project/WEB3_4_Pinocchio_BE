cecdab8c985d6a718eb3df38a4712c2500e69ba2
Hoon <xianse10@gmail.net>
2025-04-09 14:24:32 +0900

    s3는 aws 에서 키를 직접 주입

--- src/main/java/sns/pinocchio/config/S3img/S3Uploader.java
+++ src/main/java/sns/pinocchio/config/S3img/S3Uploader.java
@@ -1,12 +1,10 @@
 package sns.pinocchio.config.S3img;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -20,29 +18,23 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class S3Uploader {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
-
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
+    // 버킷 이름은 application-dev.properties 또는 직접 문자열로 관리
+    private final String bucket = "devcos4-team09-image-bucket";
 
     public String uploadFile(MultipartFile file, String dirName) throws IOException {
         String fileName = dirName + "/" + UUID.randomUUID() + "_" + file.getOriginalFilename();
 
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
+        // IAM Role을 기반으로 AWS 자격증명 자동 주입
         S3Client s3Client = S3Client.builder()
                 .region(Region.AP_NORTHEAST_2)
-                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
 
         s3Client.putObject(PutObjectRequest.builder()
                         .bucket(bucket)
                         .key(fileName)
                         .contentType(file.getContentType())
-                        .acl("public-read")  //  퍼블릭 접근 허용!
+                        .acl("public-read")
                         .build(),
                 RequestBody.fromBytes(file.getBytes()));
 

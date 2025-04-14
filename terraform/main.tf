6f52614937fe4127963d5ad58f52f85984d2ccd2
Hoon <xianse10@gmail.net>
2025-04-14 14:59:42 +0900

    테라폼 수정

--- terraform/main.tf
+++ terraform/main.tf
@@ -372,16 +372,16 @@ services:
             retries: 5
 
     react:
-    image: jeong6/frontend-app:latest
-    container_name: react-app-container
-    restart: always
-    ports:
-      - "3000:3000"
-    networks:
-      - app-network
-    depends_on:
-      spring:
-        condition: service_healthy
+        image: jeong6/frontend-app:latest
+        container_name: react-app-container
+        restart: always
+        ports:
+          - "3000:3000"
+        networks:
+          - app-network
+        depends_on:
+          spring:
+            condition: service_healthy
 
     nginx:
         image: nginx:latest

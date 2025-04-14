67e0cca2149daf248e4150b38723449ec2240c96
gorhkd <gorhkd223@gmail.com>
2025-04-14 15:40:44 +0900

    chore : 유저 프로필 수정

--- src/main/java/sns/pinocchio/presentation/member/MemberController.java
+++ src/main/java/sns/pinocchio/presentation/member/MemberController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -48,7 +49,7 @@ public class MemberController {
   }
 
   // 유저 프로필 수정
-  @PutMapping
+  @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<ProfileResponseDto> updateMemberInfo(
       @AuthenticationPrincipal CustomUserDetails customUserDetails,
       @Valid @RequestPart("request") UpdateRequestDto updateRequestDto,
